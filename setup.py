888906819c0a54a8ebaae98eeaba937b7922b8d8
Dustin Morrill <dustin.morrill@sony.com>
2023-05-21 14:50:27 -0600

    Add to README and setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='redo',
+      version='1.0',
+      description="""\
+Utilities to define and work with the ReDO (recurrent TODO) Markdown \
+documents, and script to update ReDO files.\
+""",
+      author='Dustin Morrill',
+      author_email='dmorrill10@gmail.com',
+      packages=[],
+     )
