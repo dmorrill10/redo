f064d498292e8fc09f4581a35bb506acd360013b
Dustin Morrill <dustin.morrill@sony.com>
2023-05-22 17:29:26 -0600

    Implement Task

---
+++ redo/__init__.py
@@ -0,0 +1,35 @@
+from typing import Iterator, List
+import re
+
+
+def run_cli() -> None:
+    pass
+
+
+task_regex = re.compile(r"(^|\r|\n|\r\n)[-*]\s*")
+completion_regex = re.compile(r"^[-*]\s*(\[?x\]?)\s")
+
+
+class Task:
+    lines: List[str]
+    has_been_completed: bool
+
+    def __init__(self, text: str) -> None:
+        lines = text.splitlines()
+        assert len(lines) > 0
+        match = completion_regex.match(lines[0])
+        self.has_been_completed = bool(match and match.group(1))
+        lines[0] = (completion_regex if self.has_been_completed else task_regex).sub(
+            "", lines[0]
+        )
+        self.lines = lines
+
+    def has_been_completed(self) -> bool:
+        return False
+
+
+def each_task(text: str) -> Iterator[Task]:
+    for task in task_regex.split(text):
+        task = task.strip()
+        if len(task) > 0:
+            yield Task(task)
--- tests/test_redo.py
+++ tests/test_redo.py
@@ -2,34 +2,44 @@ import pytest
 import redo
 
 
-@pytest.mark.parametrize('task_marker', ['-', '- ', '*', '* '])
-def test_create_incomplete_task(task_marker: str) -> None:
-    first_line = 'first line of task'
-    second_line = 'second line of task'
-    incomplete_task = redo.Task(f"{task_marker}{first_line}\n{second_line}")
+@pytest.mark.parametrize("task_marker", ["-", "- ", "*", "* "])
+def test_create_task_that_has_not_been_completed(task_marker: str) -> None:
+    first_line = "first line of task"
+    second_line = "second line of task"
+    task_that_has_not_been_completed = redo.Task(
+        f"{task_marker}{first_line}\n{second_line}"
+    )
 
-    assert not incomplete_task.is_completed()
-    assert incomplete_task.lines() == [first_line, second_line]
+    assert not task_that_has_not_been_completed.has_been_completed
+    assert task_that_has_not_been_completed.lines == [first_line, second_line]
 
-@pytest.mark.parametrize('task_marker', ['-', '- ', '*', '* '])
-@pytest.mark.parametrize('completion_marker', ['x ', '[x] '])
+
+@pytest.mark.parametrize("task_marker", ["-", "- ", "*", "* "])
+@pytest.mark.parametrize("completion_marker", ["x ", "[x] "])
 def test_create_completed_task(task_marker: str, completion_marker: str) -> None:
-    first_line = 'first line of task'
-    second_line = 'second line of task'
-    completed_task = redo.Task(f"{task_marker}{completion_marker}{first_line}\n{second_line}")
+    first_line = "first line of task"
+    second_line = "second line of task"
+    completed_task = redo.Task(
+        f"{task_marker}{completion_marker}{first_line}\n{second_line}"
+    )
 
-    assert completed_task.is_completed()
-    assert completed_task.lines() == [first_line, second_line]
+    assert completed_task.has_been_completed
+    assert completed_task.lines == [first_line, second_line]
 
-@pytest.mark.parametrize('task_marker', ['-', '- ', '*', '* '])
+
+@pytest.mark.parametrize("task_marker", ["-", "- ", "*", "* "])
 def test_create_two_tasks(task_marker: str) -> None:
-    first_line = 'first line of task'
-    second_line = 'second line of task'
-    third_line = 'first line of second task'
-    tasks = redo.parse(f"{task_marker}{first_line}\n{second_line}\n{task_marker}{third_line}")
+    first_line = "first line of task"
+    second_line = "second line of task"
+    third_line = "first line of second task"
+    tasks = list(
+        redo.each_task(
+            f"{task_marker}{first_line}\n{second_line}\n{task_marker}{third_line}"
+        )
+    )
 
     assert len(tasks) == 2
 
-    assert not tasks[0].is_completed()
-    assert tasks[0].lines() == [first_line, second_line]
-    assert tasks[1].lines() == [third_line]
+    assert not tasks[0].has_been_completed
+    assert tasks[0].lines == [first_line, second_line]
+    assert tasks[1].lines == [third_line]
